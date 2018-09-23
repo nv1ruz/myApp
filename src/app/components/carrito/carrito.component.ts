6539a7ca68d3240f66bf0c1d02947e2d70e5a022
Franco <fd.martinezfranco@gmail.com>
2018-09-22 21:07:47 -0300

    carrito

--- src/app/components/carrito/carrito.component.ts
+++ src/app/components/carrito/carrito.component.ts
@@ -19,6 +19,25 @@ export class CarritoComponent implements OnInit {
       $('select').formSelect();
     });
 
+    //Cantidad
+    $(document).ready( function() { 
+      var el = $('.test'); 
+      function change( amt ) { 
+      el.val( parseInt( el.val(), 10 ) + amt ); 
+      } 
+      $('.up').click( function() { 
+        if(el.val()<10){
+          change( 1 );
+        } else{
+        }
+      } ); 
+      $('.down').click( function() { 
+        if(el.val()>1){
+          change( -1 ); 
+        }
+      } ); 
+    } ); 
+
   }
 
 }
