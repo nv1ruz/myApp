3f389d0910845e9681785e4956d1bc09bd2ed883
nv1ruz <braian.martz@gmail.com>
2018-10-12 01:07:26 -0300

    modificacion direcciones

--- src/app/components/domicilios/domicilios.component.ts
+++ src/app/components/domicilios/domicilios.component.ts
@@ -10,40 +10,55 @@ import { FirebaseService } from '../../providers/firebase.service';
 export class DomiciliosComponent implements OnInit {
  
   public visible: boolean = false;
+  public txtBtn: string = 'Agregar dirección';
   public domicilios = [];
 
   constructor( private _fs: FirebaseService ) { }
 
   ngOnInit() {
     this.getDomicilios().subscribe( (snap) => {
-      this.domicilios = [];
-      snap.forEach( (data: any) => {
-        console.log( data.payload.doc.data() );
-        this.domicilios.push({
-          id: data.payload.doc.id,
-          // data: data.payload.doc.data(),
-          calle: data.payload.doc.data().calle,
-          numero: data.payload.doc.data().numero,
-          cp: data.payload.doc.data().cp,
-          provincia: data.payload.doc.data().provincia,
-          ciudad: data.payload.doc.data().ciudad
+      console.log( snap );
+      if( snap.length > 0){
+        console.log("existen direcciones");
+        this.domicilios = [];
+        snap.forEach( (data: any) => {
+          // console.log( data.payload.doc.data() );
+          this.domicilios.push({
+            id: data.payload.doc.id,
+            // data: data.payload.doc.data(),
+            calle: data.payload.doc.data().calle,
+            numero: data.payload.doc.data().numero,
+            barrio: data.payload.doc.data().barrio,
+            cp: '5380',
+            provincia: 'La Rioja',
+            ciudad: 'Chamical'
+            // cp: data.payload.doc.data().cp,
+            // provincia: data.payload.doc.data().provincia,
+            // ciudad: data.payload.doc.data().ciudad
+          });
         });
-      });
-      console.log(this.domicilios);
+        // console.log(this.domicilios);
+      } else{
+        console.log( 'No existe ninguna direccion' );
+      }
     });
   }
 
-  guardarDomicilio( cal: string, num: string, pis: string, ent: string, ref: string, cp: string, pro: string, ciu: string ){
+  guardarDomicilio( cal: string, num: string, pis: string, ent: string, bar: string, ref: string ){
     this._fs.afs.collection( 'usuarios' ).doc( this._fs.usuario.uid )
     .collection( 'direcciones' ).add({
       calle: cal,
       numero:  num,
       pisoDepto: pis,
       entreCalles: ent,
+      barrio: bar,
       referencias: ref,
-      cp: cp,
-      provincia: pro,
-      ciudad: ciu
+      cp: '5380',
+      provincia: 'La Rioja',
+      ciudad: 'Chamical'
+      // cp: cp,
+      // provincia: pro,
+      // ciudad: ciu
     }).then( function() {
       console.log( "El documento se guardó correctamente");
     }).catch( function(error) {
@@ -57,6 +72,11 @@ export class DomiciliosComponent implements OnInit {
   }
 
   toggle(){
+    if( this.visible ){
+      this.txtBtn = 'Agregar dirección';
+    } else{
+      this.txtBtn = 'Volver';
+    }
     this.visible = !this.visible;
   }
 
